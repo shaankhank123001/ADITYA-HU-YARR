bb04c85ca79fbe80e1f8a8aa88a940b912ff6b7c
shaankhank123001 <shaankhank42@gmail.com>
2024-12-20 21:01:06 +0530

    Update 0_ARYAN-SAMOSE.js

--- Priyansh/commands/0_ARYAN-SAMOSE.js
+++ Priyansh/commands/0_ARYAN-SAMOSE.js
@@ -3,7 +3,7 @@ module.exports.config = {
         name: "samose",
     version: "1.0.1",
         hasPermssion: 0,
-        credits: "ARYAN", 
+        credits: "AADI BABU", 
         description: "hihihihi",
         commandCategory: "no prefix",
         usages: "samose",
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("Samose")==0 || event.body.indexOf("samosa")==0 || event.body.indexOf("SAMOSE")==0 || event.body.indexOf("SAMOSA")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  💐𝐀𝐑𝐘𝐀𝐍💐 \n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐒𝐚𝐦𝐨𝐒𝐞\n_______________________",
+                                body: "𝐎𝐰𝐧𝐞𝐫 ➻    𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮\n────────────────────\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐒𝐚𝐦𝐨𝐒𝐞\n────────────────────\n\n",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/samosas_lover_here_|_Ricette,_Easy,_Crockpot(360P).mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);

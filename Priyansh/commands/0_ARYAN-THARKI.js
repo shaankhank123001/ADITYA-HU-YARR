f54b4fbaeb3f7a22b803bd311f1ebd5bdd4c9a17
shaankhank123001 <shaankhank42@gmail.com>
2024-12-20 20:59:30 +0530

    Update 0_ARYAN-THARKI.js

--- Priyansh/commands/0_ARYAN-THARKI.js
+++ Priyansh/commands/0_ARYAN-THARKI.js
@@ -3,8 +3,8 @@ module.exports.config = {
         name: "TharKi",
     version: "1.1.1",
         hasPermssion: 0,
-        credits: "ARYAN", 
-        description: "THIS BOT IS ARYAN",
+        credits: "AADI BABU", 
+        description: "THIS BOT IS AADI BABU",
         commandCategory: "no prefix",
     cooldowns: 5, 
 };

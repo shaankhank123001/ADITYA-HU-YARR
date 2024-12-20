b19f3a89d30698e8db690ded687c6a5d5b928dcd
shaankhank123001 <shaankhank42@gmail.com>
2024-12-20 20:06:44 +0530

    Update antiout.js

--- Priyansh/events/antiout.js
+++ Priyansh/events/antiout.js
@@ -15,8 +15,8 @@ module.exports.run = async({ event, api, Threads, Users }) => {
  if (type == "self-separation") {
   api.addUserToGroup(event.logMessageData.leftParticipantFbId, event.threadID, (error, info) => {
    if (error) {
-    api.sendMessage(`💐𝐁𝐎𝐒𝐒 𝐌 𝐈𝐒𝐄 𝐃𝐔𝐁𝐀𝐑𝐀 𝐀𝐃𝐃 𝐍𝐀𝐇𝐈 𝐊𝐀𝐑 𝐏𝐀𝐘𝐀💐 ${name} 𝐆𝐑𝐎𝐔𝐏 𝐌 :( `, event.threadID)
-   } else api.sendMessage(`💐𝐀𝐋𝐄 𝐌𝐄𝐑𝐈 𝐉𝐀𝐀𝐍 𝐁𝐇𝐀𝐆 𝐊𝐀𝐑 𝐊𝐀𝐇𝐀 𝐉𝐀 𝐑𝐀𝐇𝐄 𝐇𝐎💐, ${name} 💐𝐇𝐔𝐌 𝐀𝐏𝐊𝐎 𝐁𝐇𝐀𝐆𝐍𝐄 𝐍𝐀𝐇𝐈 𝐃𝐄𝐍𝐆𝐄 𝐉𝐀𝐀𝐍,𝐃𝐄𝐊𝐇𝐎 𝐉𝐀𝐀𝐍 𝐌𝐄𝐍𝐄 𝐀𝐏𝐊𝐎 𝐏𝐇𝐈𝐑 𝐀𝐃𝐃 𝐊𝐀𝐑 𝐃𝐈𝐘𝐀😘💐`, event.threadID);
+    api.sendMessage(`Isse Dubara Add Nhi Kar Paya 🥺 ${name} Group Mai :( `, event.threadID)
+   } else api.sendMessage(`Bhag Ke Jaane Ka Nhi, ${name} Baby, Dekho Phir Se Add Kardiya Aapko`, event.threadID);
   })
  }
 }
